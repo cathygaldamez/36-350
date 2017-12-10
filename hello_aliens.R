9e45c0e24e9b73a8c3a2ac8c30f05659d1e2b098
cathygaldamez <33264281+cathygaldamez@users.noreply.github.com>
2017-12-09 20:07:36 -0500

    Delete hello_aliens.R

--- hello_aliens.R
+++
@@ -1,5 +0,0 @@
-Hello aliens!
-
-Earth is a little crazy right now with global warming, racism, the slave trade, etc. So I don't think now is a good time. But how did you guys even find Earth or travel to Earth. To travel all those light years must be extremely expensive. Also, how much more advanced is your civilization? Compared to other exoplanets, Earth is extremely young. Anyways, maybe you guys can help us out.
-
-Peace out. 
